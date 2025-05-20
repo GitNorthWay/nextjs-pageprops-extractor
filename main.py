6179a4bc7917b427210f6cbeea51842755c3bfd1
Ricardo van Noort <rmvannoort@hotmail.com>
2025-05-20 10:59:05 +0200

    Read website details from file.

--- main.py
+++ main.py
@@ -3,15 +3,16 @@
 Example script demonstrating how to use the next_data_extractor module
 to extract pageProps data from Next.js pages and save them to JSON files.
 """
-
+from utils.generic import read_json_file
 from utils.next_data_extractor import extract_next_data
 
 
 if __name__ == "__main__":
     # Extract pageProps from a single page and save to a JSON file
     print("\n Extract pageProps from a single page and save to a JSON file")
-    homepage_url = "https://www.example.com"
-    single_page_url = "https://www.example.com/page1"
+    website_details = read_json_file("website_details.json")
+    homepage_url = website_details["homepage_url"]
+    single_page_url = website_details["page_url"]
 
     # Extract data and save to a JSON file
     page_props = extract_next_data(homepage_url, single_page_url, True)
